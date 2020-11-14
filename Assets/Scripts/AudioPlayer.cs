5f0de84b52c846dcccbf2af7bb8d22260b35f1a4
DecaIi <bastien.porte@telecom-sudparis.eu>
2020-11-14 23:24:58 +0100

    Add audio effect

--- Assets/Scripts/AudioPlayer.cs
+++ Assets/Scripts/AudioPlayer.cs
@@ -8,39 +8,33 @@ public class AudioPlayer : MonoBehaviour
 
     public AudioClip takeStell;
     [SerializeField] 
-    int volumeTakeStell;
+    float volumeTakeStell;
 
     [SerializeField]
     bool play1 = false; //for testing 
 
     public AudioClip dropStell;
     [SerializeField]
-    int volumeDropStell;
+    float volumeDropStell;
     [SerializeField]
     bool play2 = false;//for testing 
     
     public AudioClip DropPlayerSound;
     [SerializeField]
-    int volumeDropPlayerSound;
+    float volumeDropPlayerSound;
     [SerializeField]
     bool play3 = false;//for testing 
 
     public AudioClip validationClip;
     [SerializeField]
-    int volumeValidation;
+    float volumeValidation;
     [SerializeField]
     bool play4 = false;//for testing 
 
-    public AudioClip themeClip;
-    [SerializeField] float volume;
-
-
-    // Start is called before the first frame update
+     // Start is called before the first frame update
     void Start()
     {
         this.audioSource = GetComponent<AudioSource>();
-        audioSource.Play();
-
     }
     private void FixedUpdate()
     {
