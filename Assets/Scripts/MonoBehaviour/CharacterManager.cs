3ed3e2b50126df6db846fe8a13ceeae738929edd
Romain Tirbisch <elf0@macbook-pro-de-romain.home>
2020-11-14 21:40:18 +0100

    characterManager and level_SO partial

--- Assets/Scripts/MonoBehaviour/CharacterManager.cs
+++ Assets/Scripts/MonoBehaviour/CharacterManager.cs
@@ -6,4 +6,11 @@ public class CharacterManager : MonoBehaviour
 {
     [SerializeField]
     private Level_SO level;
+
+    public void NextCharacter()
+    {
+        GameObject character = Instantiate(this.level.CharacterPrefab, this.level.InitialPosition, Quaternion.identity);
+
+        
+    }
 }
