aa04ce27c2193ac596e4bfeaf89a4095c0cb0a0d
DecaIi <bastien.porte@telecom-sudparis.eu>
2020-11-14 20:44:51 +0100

    add slidingbare

---
+++ Assets/Scripts/MovingBare.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MovingBare : MonoBehaviour
+{
+    [SerializeField]  private int value ; //should be pass at nomrla field afeter test 
+    [SerializeField]  private int maxvalue;
+    private Image bar;
+
+    void Start()
+    {
+        bar = GetComponent<Image>();
+    }
+
+    private void Update() //to remove 
+    {
+        UpdateBareState();
+    }
+
+    // Make the bare avancement decrease 
+    public void ReduceBare(int value)
+    {
+        this.value -= value;
+        UpdateBareState();
+    }
+    // Make the bare avancement incereace 
+    public void ImproveBare(int value)
+    {
+        this.value += value;
+        UpdateBareState();
+    }
+
+    //acutualise l'etas de la bar 
+    private void UpdateBareState()
+    {
+        value = Mathf.Clamp(value, 0, maxvalue);
+
+        float amount = (float)value / maxvalue;
+        Debug.Log(amount);
+        bar.fillAmount = amount;
+    }
+}
