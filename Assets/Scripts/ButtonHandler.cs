0f0eed5d8362d572b48cb874aef85c88fca5c325
florianbsh <florian.bescher@telecom-sudparis.eu>
2020-11-15 11:30:46 +0100

    Validation button working

---
+++ Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class ButtonHandler : MonoBehaviour
+{
+    [SerializeField]
+    private Level_SO level;
+
+    [SerializeField]
+    private Button validationButton;
+
+    public UnityEvent onNextBatch;
+
+    private void Awake()
+    {
+        Debug.Log("disabled");
+        validationButton.gameObject.SetActive(false);
+    }
+
+    public void OnDropped(bool isFirstTime)
+    {
+        if (this.level.isBatchEnd())
+        {
+            validationButton.gameObject.SetActive(true);
+        }
+    }
+
+    public void ValidateBatch()
+    {
+        onNextBatch.Invoke();
+        validationButton.gameObject.SetActive(false);
+    }
+}
