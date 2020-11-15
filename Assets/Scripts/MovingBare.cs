48e4cba32404f6ecea53f4a9a7c189c45f9362c6
DecaIi <bastien.porte@telecom-sudparis.eu>
2020-11-15 10:38:06 +0100

    jauge now sliding over time

--- Assets/Scripts/MovingBare.cs
+++ Assets/Scripts/MovingBare.cs
@@ -4,39 +4,48 @@ using UnityEngine;
 using UnityEngine.UI;
 public class MovingBare : MonoBehaviour
 {
-    [SerializeField]  private int value ; //should be pass at nomrla field afeter test 
+    [SerializeField]  private int cibleValue ; //should be pass at nomrla field afeter test 
     [SerializeField]  private int maxvalue;
+    [SerializeField] private float movingSpeed = 1 ;
+    private float curentvalue; 
     private Image bar;
 
     void Start()
     {
         bar = GetComponent<Image>();
+        curentvalue = 0;
     }
 
-    private void Update() //to remove 
+    private void FixedUpdate()
+     //to remove 
     {
+        if (curentvalue != cibleValue)
+        {
+            curentvalue = Mathf.Lerp(curentvalue, cibleValue, Time.time * movingSpeed);
+        }
+        
         UpdateBareState();
     }
 
     // Make the bare avancement decrease 
     public void ReduceBare(int value)
     {
-        this.value -= value;
-        UpdateBareState();
+        this.cibleValue -= value;
+       // UpdateBareState();
     }
     // Make the bare avancement incereace 
     public void ImproveBare(int value)
     {
-        this.value += value;
-        UpdateBareState();
+        this.cibleValue += value;
+        //UpdateBareState();
     }
 
     //acutualise l'etas de la bar 
     private void UpdateBareState()
     {
-        value = Mathf.Clamp(value, 0, maxvalue);
+        curentvalue = Mathf.Clamp(curentvalue, 0, maxvalue);
 
-        float amount = (float)value / maxvalue;
+        float amount = (float)curentvalue / maxvalue;
         Debug.Log(amount);
         bar.fillAmount = amount;
     }
