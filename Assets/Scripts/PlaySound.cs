fcfd89448f2c48011ca253bf8a601ea221bf4241
DecaIi <bastien.porte@telecom-sudparis.eu>
2020-11-15 11:59:52 +0100

    try ton put sound on mouse over

---
+++ Assets/Scripts/PlaySound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+    [SerializeField]
+    AudioClip onMouseOverSound;
+    [SerializeField]
+    AudioSource audioSource;
+    [SerializeField]
+    int volumeMouseOverSound;
+    // Start is called before the first frame update
+
+    private void OnMouseOver()
+    {
+        audioSource.PlayOneShot(onMouseOverSound, volumeMouseOverSound);
+    }
+}
