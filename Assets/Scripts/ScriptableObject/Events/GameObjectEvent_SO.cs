8d12f1034dbeb45e1a41292eaa94fb7c5da35467
Romain Tirbisch <elf0@macbook-pro-de-romain.home>
2020-11-15 15:58:06 +0100

    Next batch

---
+++ Assets/Scripts/ScriptableObject/Events/GameObjectEvent_SO.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Events/GameObjectEvent")]
+public class GameObjectEvent_SO : ScriptableObject
+{
+    private List<GameObjectEventListener> listeners = new List<GameObjectEventListener>();
+
+    public void Raise(GameObject value)
+    {
+        for (int i = this.listeners.Count - 1; i >= 0; --i)
+        {
+            this.listeners[i].OnEventRaised(value);
+        }
+    }
+
+    public void RegisterListener(GameObjectEventListener listener)
+    {
+        this.listeners.Add(listener);
+    }
+
+    public void UnregisterListener(GameObjectEventListener listener)
+    {
+        this.listeners.Remove(listener);
+    }
+}
